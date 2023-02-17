accfc0da8dd252941c00c96c011230274119bcb7
Pat Sissons <patricksissons@gmail.com>
2023-02-17 11:29:09 -0800

    UI infra

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,17 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
   ],
   plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
+  ignorePatterns: ['*.cjs', '*.js', '*.mjs'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   settings: {
     'svelte3/typescript': () => require('typescript'),
   },
   parserOptions: {
+    project: ['./tsconfig.json'],
     sourceType: 'module',
     ecmaVersion: 2020,
   },
@@ -21,5 +23,11 @@ module.exports = {
     browser: true,
     es2017: true,
     node: true,
+  },
+  rules: {
+    // these rules interfere with svelte's loader signatures (e.g., PageLoad)
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
   },
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="flex flex-col gap-5 items-center justify-center w-screen h-screen">
+  <h1 class="text-9xl">moji.place</h1>
+  <p class="text-lg">Stupidly simple place to find emojis.</p>
+  <p class="text-slate-500">coming soon.</p>
+</div>
